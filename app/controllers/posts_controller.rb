2ac7d1c6505a060e1b58c191172953849e85c95b
Huy Tuan <154073309+huyt39@users.noreply.github.com>
2025-08-06 15:30:46 +0700

    crud post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end 
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update 
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @post.destroy
+    redirect_to posts_path 
+  end
+
+private
+  def post_params
+    params.expect(post: [:title])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
